982cf8bf3fcd684f3f370406ba8bd2a75c74ae2c
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2022-12-02 14:28:00 +0200

    Update test-lab4.s

--- test-lab4.s
+++ test-lab4.s
@@ -1,171 +1,102 @@
-;# sa se afiseze cat si rest din media aritmectica din vector
+# sa se afiseze cat si rest din media aritmectica din vector
 
 ;# ex : 6   1 4 9 10 4 5     (5,3)    pt ca suma e 33
 
 .data
-
 n: .space 4
-
 a: .space 4
-
 s: .space 4
-
 cat: .space 4
-
 rest: .space 4
-
 sir: .space 800
-
 fs1: .asciz "%ld"
-
 print: .asciz "%ld\n"
 
 .text
-
-
-
 .global main
 
 main:
-
-
-
+; # citire numarul n
 pushl $n
-
 push $fs1
-
 call scanf
-
 popl %ebx
-
 popl %ebx
-
-
 
 lea sir, %esi
-
 xorl %ecx, %ecx
 
 for_citire:
-
     cmp n, %ecx
-
     je exit_for_citire
 
-
-
     pusha
-
     pushl $a
-
     push $fs1
-
     call scanf
-
     popl %ebx
-
     popl %ebx
-
     popa
 
-
-
     movl a, %eax
-
     movl %eax, (%esi, %ecx, 4)
-
     incl %ecx
-
     jmp for_citire
 
 exit_for_citire:
 
 
-
-
-
 xorl %ecx, %ecx
-
 movl %ecx, s
-
 lea sir, %esi
 
 loop:
 
       ;# ecx este 1, 4, 9, 10, 4, 5
 
-
-
     cmp n, %ecx
-
     je end_loop
 
-    xorl %edx,%edx
-
-    movl (%edi,%ecx,4), %eax
-
-    addl s, %ebx
-
-    movl %ecx, %ebx
-
-    
+    movl (%esi,%ecx,4), %eax
+    addl %eax, s
+    movl %ecx, %eax
 
 incl %ecx
-
 jmp loop
 
 end_loop:
 
 
-
-
-
 lea sir, %esi
-
 xorl %ecx, %ecx
 
 movl n,%ebx
-
 movl s, %eax
-
 movl $0, %edx
 
 divl %ebx
-
 movl %eax, cat
-
 movl %edx, rest
 
 
 
-pushl $cat
-
+pushl cat
 pushl $print
-
 call printf
-
+popl %ebx
 popl %ebx
 
-popl %ebx
 
 
-
-pushl $rest
-
+pushl rest
 pushl $print
-
 call printf
-
 popl %ebx
-
 popl %ebx
 
 
 
 exit:
-
 movl $1, %eax
-
 xorl %ebx, %ebx
-
 int $0x80
