bea6054bcb7de78cf4f9d32f389f8ad632585408
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2022-12-25 21:55:42 +0200

    Update tema.s
    Point 'a' and point 'b' but with mres printed out as well.

--- tema.s
+++ tema.s
@@ -12,6 +12,14 @@ p: .space 4
 
 k: .space 4
 
+garbage1: .space 4
+
+garbage2: .space 4
+
+garbage3: .space 4
+
+garbage4: .space 4
+
 st: .space 4
 
 dr: .space 4
@@ -60,75 +68,199 @@ matrix_mult:
 
     movl %eax, %edi         ;# setam edi la n-1
 
-    xorl %ecx, %ecx         
+    xorl %ecx, %ecx
+
+    movl %esi, 24(%ebp)  
 
 
 
-row_loop:
-
-    ;# verifica daca am fost in toate liniile
-
-    cmp $0, %esi          ;# am intrat in toate liniile?
-
-    jl end_row_loop      ;# if esi < 0, jump end_row_loop
 
 
+all_loop:
 
-    movl %eax, %edi         ;# reseteaza edi la n-1
+	movl 24(%ebp), %ecx
 
+	cmp $0, %ecx
 
+	jl end_all_loop
 
-    column_loop:
+	xorl %ecx, %ecx
 
-	    ;# verifica daca am fost in toate coloanele 
+	movl 20(%ebp), %eax 
 
-	    cmp $0, %edi       ;# am intrat in toate coloanele?
+    	decl %eax
 
-	    jl end_column_loop   ;# if edi < 0, jump end_column_loop
+	movl %eax, %esi         
 
-
-
-	    ;# calculeaza mres[esi][edi] = m1[esi][edi] * m2[edi][esi]
-
-	    movl 12(%ebp), %ebx       ;# in ebx sa fie adresa lui m2
-
-	    movl 8(%ebp), %edx        ;# in edx sa fie adresa lui m1
-
-	    movl (%ebx, %edi, 4), %eax      ;# m2[edi][esi] 
-
-	    mull (%edx, %esi, 4)         ;# m1[esi][edi] * m2[edi][esi]
-
-	    addl %eax, %ecx                ;# tinem minte in ecx
-
-
-
-	    decl %edi              ;# decremenreaza nr coloanei
-
-	    jmp column_loop      ;# continua cu urmatoarea coloana
-
-
-
-    end_column_loop:
+        movl %eax, %edi
 
     
 
-    ;# tinem minte rezultatul in mres[esi][esi]
+	row_loop:
 
-    movl 16(%ebp), %ebx     ;# get mres
+	    ;# verifica daca am fost in toate liniile
 
-    movl %ecx, (%ebx, %esi, 4)         ;# tinem minte rezultatul in mres[esi][esi]
+	    cmp $0, %esi          ;# am intrat in toate liniile?
 
-
-
-    xorl %ecx, %ecx         
-
-    decl %esi              ;# decrementam nr de coloane
-
-    jmp row_loop         ;# continua la linia urmatoare
+	    jl end_row_loop      ;# if esi < 0, jump end_row_loop
 
 
 
-end_row_loop:
+	    movl %eax, %edi         ;# reseteaza edi la n-1
+
+	    movl %esi, 28(%ebp)
+
+	    
+
+	    column_loop:
+
+		    ;# verifica daca am fost in toate coloanele 
+
+		    cmp $0, %edi       ;# am intrat in toate coloanele?
+
+		    jl end_column_loop   ;# if edi < 0, jump end_column_loop
+
+
+
+		    ;# calculeaza mres[esi][edi] = m1[esi][edi] * m2[edi][esi]
+
+		    movl 12(%ebp), %ebx       ;# in ebx sa fie adresa lui m2
+
+		    movl 8(%ebp), %ecx        ;# in ecx sa fie adresa lui m1
+
+		    
+
+		    movl 28(%ebp), %esi
+
+		    movl %edi, %eax
+
+	    	    movl $0, %edx       
+
+		    mull 20(%ebp)
+
+		    addl %esi, %eax
+
+		    
+
+		    movl (%ebx, %eax, 4), %ebx      ;# m2[edi][esi] 
+
+		    
+
+		    
+
+		    movl 24(%ebp), %eax
+
+	    	    movl $0, %edx       
+
+		    mull 20(%ebp)
+
+		    addl %edi, %eax
+
+		    movl %eax, %esi
+
+		    
+
+		    movl %ebx, %eax
+
+		    movl $0, %edx   
+
+		    mull (%ecx, %esi, 4)         ;# m1[24(%ebp)][edi] * m2[edi][esi]
+
+		    subl %edi, %esi
+
+		    movl %eax, %ebx
+
+		    movl %esi, %eax
+
+		    movl $0, %edx
+
+		    divl 20(%ebp)
+
+		    movl %eax, %esi
+
+		    movl %ebx, %eax
+
+		    
+
+		    movl 16(%ebp), %ebx     ;# get mres
+
+		    movl %eax, %ecx
+
+		    movl 24(%ebp), %eax
+
+		    movl $0, %edx
+
+		    mull 20(%ebp)
+
+		    movl 28(%ebp), %esi
+
+		    addl %esi, %eax
+
+		    movl %eax, %esi
+
+		    movl %ecx, %eax
+
+		    addl %eax, (%ebx, %esi, 4)         ;# tinem minte rezultatul in mres[24(%ebp)][esi]
+
+		    movl 20(%ebp), %eax
+
+		    movl 28(%ebp), %esi
+
+		    
+
+		    decl %edi              ;# decremenreaza nr coloanei
+
+		    jmp column_loop      ;# continua cu urmatoarea coloana
+
+
+
+		   
+
+		     
+
+
+
+
+
+	    end_column_loop:
+
+	    
+
+	    
+
+	    decl %esi              ;# decrementam nr de coloane
+
+	    movl 20(%ebp), %eax
+
+	    decl %eax
+
+	    movl %esi, 28(%ebp)
+
+	    jmp row_loop         ;# continua la linia urmatoare
+
+
+
+	end_row_loop:
+
+	
+
+	movl 24(%ebp), %ecx
+
+	decl %ecx
+
+	movl %ecx, 24(%ebp)
+
+	xorl %ecx, %ecx
+
+	jmp all_loop
+
+	
+
+	
+
+	
+
+end_all_loop:
 
     popl %ebp              ;# refacem base pointer-ul
 
@@ -490,7 +622,15 @@ chestie:
 
 
 
+;# daca k=1
+
+
+
 movl $1, %edx
+
+cmp k, %edx
+
+je affi
 
 
 
@@ -499,6 +639,78 @@ for_drum:              ;# mres-->%ecx     m2-->%ebx    m1-->%eax
 	cmp k, %edx
 
 	je afi
+
+	
+
+	;# trebuie initializat mres cu 0
+
+	
+
+	movl %edx, garbage1  ;# salvam %eax si %edx pentru ca ii "stricam" si avem nevoie de ei intacti dupa
+
+	movl %eax, garbage2
+
+	xorl %edi, %edi
+
+	movl %edi, garbage3
+
+	
+
+	init_0_row:
+
+		cmp n, %edi
+
+		je end_init_0_row
+
+		
+
+		xorl %esi, %esi
+
+		
+
+		init_0_column:
+
+			cmp n, %esi
+
+			je end_init_0_column
+
+			
+
+			movl %edi, %eax
+
+			movl $0, %edx
+
+			mull n
+
+			addl %esi, %eax
+
+			movl $0, (%ecx, %eax, 4)    ;# mres[%edi][%esi]=0
+
+			
+
+			incl %esi
+
+			jmp init_0_column
+
+		
+
+		end_init_0_column:
+
+		
+
+		incl %edi
+
+		jmp init_0_row
+
+			
+
+	end_init_0_row:
+
+	
+
+	
+
+	movl garbage2, %eax
 
 	
 
@@ -520,7 +732,89 @@ for_drum:              ;# mres-->%ecx     m2-->%ebx    m1-->%eax
 
 	
 
-	movl %ecx, %eax
+	;# incarcam in m1, care este la adresa %eax, tot ce este in mres, care este la adresa %ecx
+
+	
+
+	xorl %edi, %edi
+
+	for_copy_row:
+
+		cmp n, %edi
+
+		je end_for_copy_row
+
+		
+
+		xorl %esi, %esi
+
+		
+
+		for_copy_column:
+
+			cmp n, %esi
+
+			je end_for_copy_column
+
+			
+
+			xorl %edx, %edx
+
+			
+
+			for_imul:
+
+				cmp n, %edx
+
+				je end_for_imul
+
+				
+
+				addl %edi, garbage3
+
+				incl %edx
+
+				jmp for_imul
+
+			
+
+			end_for_imul:
+
+			
+
+			addl %esi, garbage3
+
+			movl %esi, garbage4
+
+			movl garbage3, %edx
+
+			movl (%ecx, %edx, 4), %esi
+
+			movl %esi, (%eax, %edx, 4)
+
+			movl garbage4, %esi
+
+			incl %esi
+
+			jmp for_copy_column
+
+		
+
+		end_for_copy_column:
+
+		
+
+		incl %edi
+
+		jmp for_copy_row
+
+			
+
+	end_for_copy_row:
+
+	
+
+	movl garbage1, %edx
 
 	incl %edx
 
@@ -528,7 +822,11 @@ for_drum:              ;# mres-->%ecx     m2-->%ebx    m1-->%eax
 
 	
 
-afi:
+affi:   ;# cazul in care k=1
+
+
+
+movl %eax, %ecx
 
 
 
@@ -541,8 +839,6 @@ afi:
 	mull n
 
 	addl dr, %eax
-
-	lea mres, %ecx
 
 	movl (%ecx, %eax, 4), %edx
 
@@ -561,6 +857,46 @@ call printf
 popl %ebx
 
 popl %ebx
+
+	
+
+	
+
+afi:
+
+
+
+;#implementam in matrice unde left-->st si right-->dr    adica mres[st][dr]
+
+    	movl st, %eax
+
+    	movl $0, %edx       
+
+	mull n
+
+	addl dr, %eax
+
+	movl (%ecx, %eax, 4), %edx
+
+	
+
+;# afisarea valorii cerute
+
+
+
+pushl %edx
+
+pushl $afis
+
+call printf
+
+popl %ebx
+
+popl %ebx
+
+
+
+
 
 
 
